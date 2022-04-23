fe61e54c7f1a7bd7ff6c7a87f9bc488b74593a5d
RubelMozumder <rubelmozumder@outlook.com>
2022-04-23 17:07:14 +0200

    fixgin bug: add code in main.cpp to take ending point(x,y)

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,20 @@ int main(int argc, const char** argv) {
       osm_data = std::move(*data);
   }
 
+  // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+  // user input for these values using std::cin. Pass the user input to the
+  // RoutePlanner object below in place of 10, 10, 90, 90.
+
   float start_x, start_y, end_x, end_y;
   std::cout << "Enter the starting location (x,y), x = ";
   std::cin >> start_x;
   std::cout << "y = ";
   std::cin >> start_x;
+  std::cout << "Enter the ending point : \n x = ";
+  std::cin >> end_x;
+  std::cout << "y = ";
+  std::cin >> end_y;
+  // jhfjhj
   // Build Model.
   RouteModel model{osm_data};
 
